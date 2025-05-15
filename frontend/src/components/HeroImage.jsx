7d434b570a37763dbe1e7716e2401a5905744a35
ItzDaniell <juan.rodriguez.o@tecsup.edu.pe>
2025-05-14 22:03:36 -0500

    Auth Image and Hero Image Component

---
+++ frontend/src/components/HeroImage.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import authImage from '../assets/images/authFondo.webp';
+
+const HeroImage = () => {
+  return (
+    <div className='d-flex auth-hero-image-container'>
+        <img 
+            src={authImage}
+            alt="Imagen de fondo"
+            className='auth-hero-image'
+        />
+    </div>
+  );
+};
+
+export default HeroImage;  
