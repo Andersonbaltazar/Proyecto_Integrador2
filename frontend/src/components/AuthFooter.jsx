183935a8f995379d4d044e6c61f7acc2948e4f8d
ItzDaniell <juan.rodriguez.o@tecsup.edu.pe>
2025-05-14 22:08:32 -0500

    AuthFooter

---
+++ frontend/src/components/AuthFooter.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const AuthFooter = () => {
+  return (
+    <div className="d-flex gap-2 auth-footer">
+      <a className='auth-footer-link' href="#">Privacy</a>
+      <a className='auth-footer-link' href="#">Terms</a>
+      <a className='auth-footer-link' href="#">Nombre</a>
+      <a className='auth-footer-link' href="#">X</a>
+      <a className='auth-footer-link' href="#">GitHub</a>
+    </div>
+  );
+};
+
+export default HeroImage;  
