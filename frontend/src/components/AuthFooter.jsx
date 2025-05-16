b1a9727466fbc4b1ced931bf08aa09de2abf1f2b
ItzDaniell <juan.rodriguez.o@tecsup.edu.pe>
2025-05-15 22:59:29 -0500

    Auth Form Style

--- frontend/src/components/AuthFooter.jsx
+++ frontend/src/components/AuthFooter.jsx
@@ -2,14 +2,18 @@ import React from 'react';
 
 const AuthFooter = () => {
   return (
-    <footer className="d-flex auth-footer" aria-label="Enlaces de pie de página">
-        <nav className='d-flex gap-2 auth-footer-nav'>
-            <ul className="d-flex gap-2 list-unstyled m-0 p-0">
-            <li><a className="auth-footer-link" href="#">Privacy</a></li>
-            <li><a className="auth-footer-link" href="#">Terms</a></li>
-            <li><a className="auth-footer-link" href="#">Nombre</a></li>
-            <li><a className="auth-footer-link" href="#">X</a></li>
-            <li><a className="auth-footer-link" href="#">GitHub</a></li>
+    <footer className="d-flex auth-footer justify-center" aria-label="Enlaces de pie de página">
+        <nav className='d-flex auth-footer-nav justify-center'>
+            <ul className="d-flex auth-footer-list m-0 p-0 justify-between">
+              <div className="d-flex gap-4 group-link">
+                <li><a className="auth-footer-link" href="#">Privacy</a></li>
+                <li><a className="auth-footer-link" href="#">Terms</a></li>
+              </div>
+              <li><a className="auth-footer-link" href="#">Nombre</a></li>
+              <div className="d-flex gap-4 group-link">
+                <li><a className="auth-footer-link" href="#">X</a></li>
+                <li><a className="auth-footer-link" href="#">GitHub</a></li>
+              </div>
             </ul>
         </nav>
     </footer>
--- frontend/src/components/AuthLayout.jsx
+++ frontend/src/components/AuthLayout.jsx
@@ -5,35 +5,24 @@ import HeroImage from "./HeroImage";
 
 const AuthLayout = () => {
   return (
-    <div className="d-flex flex-column auth-layout">
-      <div className="d-flex flex-column auth-layout-container">
-        <header className="d-flex flex-column auth-layout-header">
-            <h1 className="title-auth-layout">¡Bienvenido!</h1>
-            <p className="gretting-auth-layout">
+    <div className="d-flex auth-layout justify-center">
+      <div className="d-flex flex-column auth-layout-container justify-center align-center gap-5">
+        <header className="d-flex flex-column auth-layout-header gap-2 align-center">
+            <h1 className="title-auth-layout text-center">¡Bienvenido!</h1>
+            <p className="gretting-auth-layout text-center">
                 Bienvenido a <strong>Nombre</strong>. Ingresa a nuestra aplicación
                 usando una de las opciones.
             </p>
         </header>
-        <section className="list-auth-buttons gap-2 d-flex flex-column">
+        <section className="list-auth-buttons gap-2 d-flex flex-column align-center">
           <AuthProviderButton provider="Google" />
           <AuthProviderButton provider="Facebook" />
           <AuthProviderButton provider="X" />
           <AuthProviderButton provider="Microsoft" />
         </section>
         <section className="info-text-auth-layout">
-            <p className="">
-                Al ingresar mediante un proveedor, solo accederemos a la información
-                básica de tu perfil...
-                <br />
-                Se aplica nuestra{" "}
-                <a href="#" className="underline">
-                Política de Privacidad
-                </a>{" "}
-                y{" "}
-                <a href="#" className="underline">
-                Términos de Servicio
-                </a>
-                .
+            <p className="paragraph-auth-layout text-center">
+                Al ingresar mediante un proveedor, solo accederemos a la información básica de tu perfil (como tu nombre y correo electrónico) para crear tu cuenta y tendras acceso a todos nuestros servicios.
             </p>
         </section>
         <AuthFooter />
