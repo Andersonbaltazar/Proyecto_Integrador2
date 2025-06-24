c5a4f8170ddddb8b7bcb56bbe417fada4c45fbdc
ItzDaniell <juan.rodriguez.o@tecsup.edu.pe>
2025-06-23 20:24:34 -0500

    Refactor button components in CropsList and AuthProviderButton; add cursor pointer style in components.css

--- frontend/src/components/widgets/AuthProviderButton.jsx
+++ frontend/src/components/widgets/AuthProviderButton.jsx
@@ -1,5 +1,6 @@
 import googleLogo from '../../assets/logos/google.webp';
 import useAuthStore from '../../store/useAuthStore';
+import Button from './Button';
 
 const logos = {
   Google: googleLogo,
@@ -16,10 +17,10 @@ const AuthProviderButton = ({ provider }) => {
   };
 
   return (
-    <button className="d-flex gap-5 auth-button align-center p-5" onClick={handleLogin}>
+    <Button className="d-flex gap-5 auth-button align-center p-5" onClick={handleLogin}>
       <img className="img-logo-auth-button" src={logo} alt={`${provider} logo`} />
       <p className='fw-500 fs-4'>Continuar con {provider}</p>
-    </button>
+    </Button>
   );
 };
 
